dcc499c91dafe6f5c81dd03d494602fd6dae9a81
12Kris <116078485+12Kris@users.noreply.github.com>
2024-10-15 20:16:17 +0300

    Add buyers and blog pages

---
+++ app/components/content-box/content-box.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+export default function ContentBox({ imageSrc, date, title, description }) {
+    return (
+        <div className="bg-[#F2F1EE] h-[420px] w-[345px] overflow-hidden
+         transition-transform duration-700 ease-out hover:-translate-y-2 ">
+            <div className="relative">
+                <Image src={imageSrc} alt="jewelry-pics" className="h-60 w-full object-cover"/>
+                <div className="bg-[#F2F1EE] h-6 w-18 absolute top-0 px-2 z-10">{date}</div>
+            </div>
+            <div className="px-5 py-3">
+                <h1 className="text-lg font-medium mb-2">{title}</h1>
+                <p className="text-xs mb-4 text-gray-500">{description}</p>
+                <button>
+                    <p className="uppercase text-sm font-semibold hover:text-[#d4b486] duration-300"> Details</p>
+                </button>
+            </div>
+        </div>
+    );
+}
--- app/for-customers/page.tsx
+++ app/for-customers/page.tsx
@@ -1,11 +1,29 @@
 import BreadCrumbsBar from "../components/bread-crumbs-bar/bread-crumbs-bar";
 import PageHeading from "../components/page-heading/page-heading";
+import InformationBox from "@/app/components/information-box/information-box";
+import AboutUs from "../../public/images/for-customers/about-us.webp";
+import Delivery from "../../public/images/for-customers/delivery.webp";
+import PublicOffer from "../../public/images/for-customers/public-offer.webp";
 
 export default function ForCustomers() {
   return (
     <div>
       <BreadCrumbsBar params={{ url: `/for-customers` }} />
       <PageHeading params={{ content: "Buyers" }} />
+        <div className="flex justify-center gap-8 flex-wrap mt-10">
+          <InformationBox
+              imageSrc={AboutUs}
+              tittle="About Us"
+          />
+          <InformationBox
+              imageSrc={Delivery}
+              tittle="Delivery and payment"
+          />
+          <InformationBox
+              imageSrc={PublicOffer}
+              tittle="Public offer"
+          />
+      </div>
     </div>
   );
 }
