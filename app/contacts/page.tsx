c1781a1c89adb51be016fb5fe84eb94c2f6b9f4c
Vadym <vadymzastavniy@gmail.com>
2024-11-06 14:07:47 +0200

    feat: update styles for contacts page

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -6,10 +6,8 @@ export default function Contacts() {
         <div>
             <BreadCrumbsBar params={{ url: `/contacts` }} />
             <PageHeading params={{ content: "Contacts" }} />
-            <div className="flex flex-col lg:flex-row gap-8 mt-8 px-8 lg:px-16">
+            <div className="flex flex-col lg:flex-row gap-8 mt-8">
                 <div className="w-full lg:w-1/2 space-y-6">
-                    <p className="text-2xl font-semibold">Contacts</p>
-
                     <div>
                         <p className="font-semibold text-lg">Call us</p>
                         <p>+38 (000) 000 00 00</p>
@@ -27,34 +25,36 @@ export default function Contacts() {
                         <p>Kyiv, street Kyiv</p>
                     </div>
                 </div>
-                <div className="w-full lg:w-1/2 space-y-4">
+                <div className="w-full lg:w-3/4 space-y-4">
                     <p className="text-2xl font-semibold">Feedback</p>
                     <form className="space-y-4">
                         <input
                             type="text"
                             placeholder="Name *"
-                            className="w-full border border-gray-300 p-2 rounded-md focus:outline-none"
+                            className="w-full border border-stone-300 placeholder-stone-300 text-sm p-2 focus:border-[--secondary-clr] focus:border-1 focus:outline-none"
                         />
                         <input
                             type="text"
                             placeholder="Phone"
-                            className="w-full border border-gray-300 p-2 rounded-md focus:outline-none"
+                            className="w-full border border-stone-300 placeholder-stone-300 text-sm p-2 focus:border-[--secondary-clr] focus:border-1 focus:outline-none"
                         />
                         <input
                             type="email"
                             placeholder="Email *"
-                            className="w-full border border-gray-300 p-2 rounded-md focus:outline-none"
+                            className="w-full border border-stone-300 placeholder-stone-300 text-sm p-2 focus:border-[--secondary-clr] focus:border-1 focus:outline-none"
                         />
                         <textarea
                             placeholder="Message"
-                            className="w-full border border-gray-300 p-2 rounded-md focus:outline-none h-24 resize-none"
+                            className="w-full border border-stone-300 placeholder-stone-300 text-sm p-2 focus:border-[--secondary-clr] focus:border-1 focus:outline-none resize-none"
                         ></textarea>
-                        <button
-                            type="submit"
-                            className="bg-gray-800 text-white py-2 px-6 rounded-md hover:bg-gray-900"
-                        >
-                            SEND
-                        </button>
+                        <div className="flex justify-end">
+                            <button
+                                type="submit"
+                                className="bg-[--primary-clr] py-3 px-8 font-bold hover:bg-[--secondary-clr] hover:text-white transition duration-300"
+                            >
+                                SEND
+                            </button>
+                        </div>
                     </form>
                 </div>
             </div>
