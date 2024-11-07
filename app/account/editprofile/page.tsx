277c1c001e9768397f71314304c73841cbde6f1b
Mykhailo <mn508678@gmail.com>
2024-11-07 12:46:22 +0100

    finishing merge of accout-page

--- app/account/editprofile/page.tsx
+++ app/account/editprofile/page.tsx
@@ -4,6 +4,8 @@ import { Textarea } from "@/app/components/textarea/textarea"
 import { ChevronDown } from "lucide-react"
 import PageHeading from "@/app/components/page-heading/page-heading"
 import Button from "@/app/components/button/button"
+import Image from "next/image"
+
 export default function EditProfilePage() {
   return (
     <div className="max-w-3xl ">
@@ -13,49 +15,46 @@ export default function EditProfilePage() {
       <form className="space-y-6">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
           <div>
-            <Label htmlFor="name">Ім'я прізвище</Label>
-            <Input id="name" className="w-full" defaultValue="Mykhailo Nyskohuz" />
+            <Label htmlFor="name">Name</Label>
+            <Input id="name" labelName="Name" type="text" className="w-full" defaultValue="Mykhailo Nyskohuz" />
           </div>
           <div>
             <Label htmlFor="email">Email</Label>
-            <Input id="email" type="email" defaultValue="mn508678@gmail.com" />
+            <Input id="email" labelName="Email" type="email" defaultValue="mn508678@gmail.com" />
           </div>
           <div>
-            <Label htmlFor="phone1">Телефон # 1</Label>
+            <Label htmlFor="phone1">Phone # 1</Label>
             <div className="relative">
               <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                <img src="/placeholder.svg?height=20&width=30" alt="Flag" className="w-5 h-3" />
+                <Image src="/icons/ua-flag.svg" width={30} height={20} alt="Flag" className="w-5 h-3" />
               </div>
               <ChevronDown className="absolute inset-y-0 right-0 flex items-center pr-3 h-full" />
-              <Input id="phone1" className="pl-10 pr-8" />
+              <Input id="phone1" labelName="Phone # 1" type="text" className="pl-10 pr-8" />
             </div>
           </div>
           <div>
-            <Label htmlFor="phone2">Телефон # 2</Label>
+            <Label htmlFor="phone2">Phone # 2</Label>
             <div className="relative">
               <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                <img src="/placeholder.svg?height=20&width=30" alt="Flag" className="w-5 h-3" />
+                <Image src="/icons/ua-flag.svg" width={30} height={20} alt="Flag" className="w-5 h-3" />
               </div>
               <ChevronDown className="absolute inset-y-0 right-0 flex items-center pr-3 h-full" />
-              <Input id="phone2" className="pl-10 pr-8" />
+              <Input id="phone2" labelName="Phone # 2" type="text" className="pl-10 pr-8" />
             </div>
           </div>
           <div>
-            <Label htmlFor="company">Компанія</Label>
-            <Input id="company" />
+            <Label htmlFor="company">Company</Label>
+            <Input id="company" labelName="Company" type="text" />
           </div>
           <div>
-            <Label htmlFor="website">Веб-сайт</Label>
-            <Input id="website" />
+            <Label htmlFor="website">Website</Label>
+            <Input id="website" labelName="Website" type="text" />
           </div>
         </div>
         <div>
-          <Label htmlFor="comments">Коментарі</Label>
-          <Textarea id="comments" rows={4} />
+          <Label htmlFor="comments">Comments</Label>
+          <Textarea id="comments" labelName="Comments" rows={4} />
         </div>
-        {/* <Button type="submit" className="w-32 bg-gray-200 text-black hover:bg-gray-300">
-          ЗБЕРЕГТИ
-        </Button> */}
         <Button
           params={{
             content: "Save",
