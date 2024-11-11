3f92753503313060a6dfa3599c24b23a01563e08
Sidoryak-Ostap <ostapsidorak@gmail.com>
2024-11-11 11:26:52 +0200

    added responsiveness for shopsidebar on small screens

--- app/components/shop-sidebar/shop-sidebar.tsx
+++ app/components/shop-sidebar/shop-sidebar.tsx
@@ -1,12 +1,40 @@
-import React from "react";
+"use client";
 import Categories from "./categories/categories";
 import Filters from "./filters/filters";
 
-const ShopSidebar = () => {
+type ShopSidebarProps = {
+  isShopSidebarVisible: boolean;
+  handleCloseShopSidebar: () => void;
+};
+
+const ShopSidebar = ({
+  isShopSidebarVisible,
+  handleCloseShopSidebar,
+}: ShopSidebarProps) => {
   return (
-    <div className="w-1/5 border border-gray-300 py-4 px-6 box-border text-textclr flex flex-col gap-5">
+    <div
+      className={`lg:w-[250px] border border-gray-300 py-4 px-6 box-border text-textclr flex-col gap-5 lg:flex lg:static fixed z-50 lg:z-0 top-0 h-[100vh] lg:h-auto w-full 
+              bg-white transition-all duration-500 ease-in-out p-5 overflow-y-auto ${
+                isShopSidebarVisible
+                  ? "right-0 opacity-100"
+                  : "right-[-100%] opacity-0"
+              } lg:right-auto lg:opacity-100`}
+    >
+      <div
+        onClick={handleCloseShopSidebar}
+        className="lg:hidden py-2 border-b border-gray-600 flex justify-end cursor-pointer mb-3"
+      >
+        <button className="text-base">X</button>
+      </div>
       <Categories />
       <Filters />
+
+      <button
+        onClick={handleCloseShopSidebar}
+        className="w-full py-2 text-black uppercase text-center bg-gray-200 font-bold lg:hidden mt-10"
+      >
+        View Products
+      </button>
     </div>
   );
 };
--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,22 +1,38 @@
+"use client";
 import PageHeading from "../components/page-heading/page-heading";
 import CardsGrid from "../components/cards/cards-grid/cards-grid";
 import BreadCrumbsBar from "../components/bread-crumbs-bar/bread-crumbs-bar";
 import ShopSidebar from "../components/shop-sidebar/shop-sidebar";
 import ProductSorting from "../components/product-sorting";
+import { useState } from "react";
 
 export default function Shop() {
+  const [isShopSidebarVisible, setIsShopSidebarVisible] =
+    useState<boolean>(false);
+
+  const handleOpenShopSidebar = () => {
+    setIsShopSidebarVisible(true);
+  };
+
+  const handleCloseShopSidebar = () => {
+    setIsShopSidebarVisible(false);
+  };
+
   return (
-    <div className="w-full">
+    <div className="w-full ">
       <BreadCrumbsBar params={{ url: "/shop" }} />
 
       <div className="flex flex-row justify-between items-center mb-10">
         <PageHeading params={{ content: "Shop" }} />
-        <ProductSorting />
+        <ProductSorting handleFilterIconClick={handleOpenShopSidebar} />
       </div>
 
       <div className="w-full flex items-start gap-5">
-        <ShopSidebar />
-        <section className="w-4/5">
+        <ShopSidebar
+          isShopSidebarVisible={isShopSidebarVisible}
+          handleCloseShopSidebar={handleCloseShopSidebar}
+        />
+        <section className="flex-1">
           <CardsGrid params={{ pageType: "shop" }} />
         </section>
       </div>
