de48a46750336263b44d946b9ce1bec8fc3269c7
malinkacat <gumnickajaliza@gmail.com>
2024-12-03 16:46:20 +0200

    Fix the add-to-cart feature (add valid quantity of products)

--- app/shop/product/products-ui/actions-buttons.tsx
+++ app/shop/product/products-ui/actions-buttons.tsx
@@ -1,16 +1,21 @@
 "use client";
+import { useState, useEffect } from "react";
+
 import ProductCounter from "./product-counter";
 import Button from "@/app/components/button/button";
 import Wishlist from "@/app/components/wishlist/wishlist";
+
 import { addToCart, isProductInCart } from "@/app/utils/cart-utils";
 import { Product } from "@/app/lib/definitions";
-import { useState, useEffect } from "react";
+
 const ActionsButtons = ({ product }: { product: Product }) => {
   const [inCart, setInCart] = useState(false);
 
   useEffect(() => {
     setInCart(isProductInCart(product.product_id));
   }, [product.product_id]);
+
+  const [quantity, setQuantity] = useState<number>(1);
 
   const handleAddToCart = () => {
     if (!inCart) {
@@ -19,7 +24,7 @@ const ActionsButtons = ({ product }: { product: Product }) => {
         title: product.product_name,
         price: product.price,
         image: product.image_url,
-        quantity: 1,
+        quantity: quantity,
         discount: product.discount || 0,
       });
       setInCart(true);
@@ -28,7 +33,7 @@ const ActionsButtons = ({ product }: { product: Product }) => {
 
   return (
     <div className="flex gap-4 items-center mb-4 flex-wrap min-h-14">
-      <ProductCounter />
+      <ProductCounter quantity={quantity} setQuantity={setQuantity} />
 
       <Button
         params={{
